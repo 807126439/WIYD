0fdd234179efa8de41ab84213bc5b568f848072a
Java2 <Java2@Java2-PC>
2017-03-31 10:45:43 +0800

    this is my first commit.

---
+++ WIYD/src/main/java/com/wiyd/App.java
@@ -0,0 +1,13 @@
+package com.wiyd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

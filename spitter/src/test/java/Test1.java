957240104cbb674b6aa6d52a4e837bb4a4c68a62
Java2 <Java2@Java2-PC>
2017-03-31 10:49:48 +0800

    aaaa

---
+++ spitter/src/test/java/Test1.java
@@ -0,0 +1,16 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		methd(a,b);
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+	private static void methd(int a, int b) {
+		a=100;
+		b=100;
+	}
+
+}

6feb5d5b726ebd673801af92d9bdf74a390dcf24
Java2 <Java2@Java2-PC>
2017-06-20 18:12:32 +0800

    20170620

--- WIYD/src/main/java/com/spr/web/Investment/controller/InvestmentController.java
+++ WIYD/src/main/java/com/spr/web/Investment/controller/InvestmentController.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -43,49 +44,39 @@ public class InvestmentController extends BaseController {
 	// 加载代码内容
 	@RequestMapping(value = "/LoadInvestment")
 	@ResponseBody
-	public AjaxJson LoadInvestment(HttpServletRequest request,String fundId) {
+	public AjaxJson LoadInvestment(HttpServletRequest request,String fundId) throws Exception {
 		
 		UrlDTO dto=new UrlDTO(fundId, 1, 100, null, null);
-		String result="";
-		try {
-			//载入文档
-			File file = new File("C:\\Users\\Java2\\Desktop\\新建文件夹\\dadad.txt");
-			
-			FileInputStream fis=new FileInputStream(file);
-			InputStreamReader isr=new InputStreamReader(fis, "utf-8");
-			BufferedReader br = new BufferedReader(isr);
-			String line=null;
-			StringBuilder allText = new StringBuilder();
-			while((line=br.readLine())!= null){
-				allText.append(line);
+		
+		byte[] messageByte=HttpUtils.sendGETRequest(dto.getUrl(), null, "utf-8");
+		String result=new String(messageByte,"utf-8");
+		//开始解析
+		String jsonData=result.substring(result.indexOf("=")+1, result.length()-1);
+		System.out.println(jsonData);
+		JSONObject jsonObject = JSONObject.fromObject(jsonData);
+		String content = jsonObject.getString("content");
+		
+		System.out.println(content);
+		
+		//使用dom4j解析数据
+		Document document=DocumentHelper.parseText(content);
+		Element root = document.getRootElement();
+		
+		System.out.println(root.getName());
+		Element tbody = root.element("tbody");
+		Iterator it = tbody.elementIterator();
+		
+		while(it.hasNext()){
+			Element tr =(Element) it.next();
+			Iterator it2 = tr.elementIterator();
+			while(it2.hasNext()){
+				Element td =(Element) it2.next();
+				System.out.println(td.getText());
 			}
-			System.out.println(allText.toString());
-//			byte[] messageByte=HttpUtils.sendGETRequest(dto.getUrl(), null, "utf-8");
-//			result=new String(messageByte,"utf-8");
-			String jsonData=result.substring(result.indexOf("="), result.length());
-			JSONObject jsonObject = JSONObject.fromObject(jsonData);
-			String content = jsonObject.getString("content");
-			
-			//使用dom4j解析数据
-			Document document=DocumentHelper.parseText(content);
-			Element root = document.getRootElement();
-			Element tbody = root.element("tbody");
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		
-		System.out.println(result);
-		
 		
 		
 
 		return new AjaxJson(this.ADD_SUCCESS_MESSAGE,AjaxJson.success);
 	}
-	
-
-	
-	
 }
